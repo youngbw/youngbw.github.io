1c9e5aa6c591dada1d4268856dee6075c69e5104
youngbw <Athero23@gmail.com>
2016-10-18 19:32:49 -0700

    first matchup

--- js/controllers/TeamController.js
+++ js/controllers/TeamController.js
@@ -1,20 +1,25 @@
 angular.module('app').controller('TeamController', function($scope, $window) {
 
-    $scope.brent = {
-        'picks': {
-            'broncos': 0
-        }
-    };
-
-    $scope.jon = {
-        'picks': {
-            'broncos': 1
-        }
-    };
-
     $scope.matchups = [
         ['broncos', 'chargers']
     ];
+
+    $scope.teams = {
+
+        'broncos': {
+            'pic': '/images/logos/broncos.svg',
+            'jon': 0,
+            'brent': 1,
+            'name': 'Denver Broncos'
+        },
+        'chargers': {
+            'pic': '/images/logos/chargers.svg',
+            'jon': 1,
+            'brent': 1,
+            'name': 'San Diego Chargers'
+        }
+
+    };
 
 
 });
