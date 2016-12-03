f6ac03cc557ec29bc7e24fa2c82958cf2f485afb
youngbw <athero23@gmail.com>
2016-12-03 11:49:55 -0800

    brent likes and dislikes are up

--- js/controllers/LikesController.js
+++ js/controllers/LikesController.js
@@ -6,39 +6,39 @@ angular.module('app').controller('LikesController', function($scope) {
 
         'brent': [
             {
-                'name': 'Todd Gurley',
-                'pic': '/images/players/todd_gurley.jpg',
-                'opponent': 'saints'
+                'name': 'Colin Kaepernick',
+                'pic': '/images/players/colin_k.jpg',
+                'opponent': 'bears'
             },
             {
-                'name': 'Thomas Rawls',
-                'pic': '/images/players/thomas_rawls.jpg',
-                'opponent': 'buccaneers'
+                'name': 'Mike Evans',
+                'pic': '/images/players/mike_evans.jpg',
+                'opponent': 'chargers'
             },
             {
-                'name': 'Kelvin Benjamin',
-                'pic': '/images/players/kelvin_benjamin.jpg',
-                'opponent': 'raiders'
+                'name': 'Andrew Luck',
+                'pic': '/images/players/andrew_luck.jpg',
+                'opponent': 'jets'
             }
         ],
 
-        'jon': [
-            {
-                'name': 'Sterling Shepard',
-                'pic': '/images/players/sterling_shepard.jpg',
-                'opponent': 'browns'
-            },
-            {
-                'name': 'Tevin Coleman',
-                'pic': '/images/players/tevin_coleman.jpg',
-                'opponent': 'cardinals'
-            },
-            {
-                'name': 'DeVante Parker',
-                'pic': '/images/players/devante_parker.jpg',
-                'opponent': 'ers'
-            }
-        ]
+        // 'jon': [
+        //     {
+        //         'name': 'Sterling Shepard',
+        //         'pic': '/images/players/sterling_shepard.jpg',
+        //         'opponent': 'browns'
+        //     },
+        //     {
+        //         'name': 'Tevin Coleman',
+        //         'pic': '/images/players/tevin_coleman.jpg',
+        //         'opponent': 'cardinals'
+        //     },
+        //     {
+        //         'name': 'DeVante Parker',
+        //         'pic': '/images/players/devante_parker.jpg',
+        //         'opponent': 'ers'
+        //     }
+        // ]
 
     };
 
@@ -46,39 +46,39 @@ angular.module('app').controller('LikesController', function($scope) {
 
         'brent': [
             {
-                'name': 'Emmanuel Sanders',
-                'pic': '/images/players/emmanuel_sanders.jpg',
-                'opponent': 'chiefs'
-            },
-            {
-                'name': 'LeGarrette Blount',
-                'pic': '/images/players/legarrette_blount.jpg',
-                'opponent': 'jets'
-            },
-            {
-                'name': 'Mohamed Sanu',
-                'pic': '/images/players/mohamed_sanu.jpg',
-                'opponent': 'cardinals'
-            }
-        ],
-
-        'jon': [
-            {
-                'name': 'T.Y. Hilton',
-                'pic': '/images/players/ty_hilton.jpg',
+                'name': 'Eli Manning',
+                'pic': '/images/players/eli_manning.jpg',
                 'opponent': 'steelers'
             },
             {
-                'name': 'Jason Witten',
-                'pic': '/images/players/jason_witten.jpg',
-                'opponent': 'redskins'
+                'name': 'Jay Ajayi',
+                'pic': '/images/players/jay_ajayi.jpg',
+                'opponent': 'ravens'
             },
             {
-                'name': 'Tyler Eifert',
-                'pic': '/images/players/tyler_eifert.jpg',
-                'opponent': 'ravens'
+                'name': 'Randall Cobb',
+                'pic': '/images/players/randall_cobb.jpg',
+                'opponent': 'texans'
             }
-        ]
+        ],
+
+        // 'jon': [
+        //     {
+        //         'name': 'T.Y. Hilton',
+        //         'pic': '/images/players/ty_hilton.jpg',
+        //         'opponent': 'steelers'
+        //     },
+        //     {
+        //         'name': 'Jason Witten',
+        //         'pic': '/images/players/jason_witten.jpg',
+        //         'opponent': 'redskins'
+        //     },
+        //     {
+        //         'name': 'Tyler Eifert',
+        //         'pic': '/images/players/tyler_eifert.jpg',
+        //         'opponent': 'ravens'
+        //     }
+        // ]
 
     };
 
