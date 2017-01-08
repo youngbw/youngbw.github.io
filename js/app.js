826a96f2bed4f94ac9b74b6b05919b28d690a736
youngbw <Athero23@gmail.com>
2017-01-08 01:14:49 -0800

    brackets up

--- js/app.js
+++ js/app.js
@@ -2,16 +2,19 @@ var myApp = angular.module('app', ['ui.router']);
 
 myApp.config(function($stateProvider, $urlRouterProvider) {
 
-    $urlRouterProvider.otherwise('/team');
+    $urlRouterProvider.otherwise('/bracket');
 
     $stateProvider
-
-        .state('/team', {
-            url: '/team',
-            templateUrl: 'components/teampage.html'
+        .state('/bracket', {
+            url: '/bracket',
+            templateUrl: 'components/bracketpage.html'
         })
-        .state('/likes', {
-            url: '/likes',
-            templateUrl: 'components/likes.html'
-        })
+        // .state('/team', {
+        //     url: '/team',
+        //     templateUrl: 'components/teampage.html'
+        // })
+        // .state('/likes', {
+        //     url: '/likes',
+        //     templateUrl: 'components/likes.html'
+        // })
 });
