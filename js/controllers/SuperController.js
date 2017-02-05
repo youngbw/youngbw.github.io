279d49587791822d776dff4822a5fde53348fba3
Dustin Young <DustinYoung@Dustins-MacBook-Pro-2.local>
2017-02-05 15:23:52 -0800

    update

--- js/controllers/SuperController.js
+++ js/controllers/SuperController.js
@@ -4,13 +4,13 @@ angular.module('app').controller('SuperController', function($scope) {
 	var colors = ['pink', 'gold', 'aqua', 'forestgreen', 'red'];
 
 	$scope.team1 = {
-		nums: [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0]],
+		nums: [[6, 7], [3, 0], [2, 5], [8, 4], [1, 9]],
 		name: 'Patriots',
 		pic: 'images/patriots_super.jpg'
 	};
 
 	$scope.team2 =  {
-		nums: [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0]],
+		nums: [[2, 3], [6, 0], [1, 9], [4, 8], [5, 7]],
 		name: 'Falcons',
 		pic: 'images/falcons_super.jpg'
 	};
