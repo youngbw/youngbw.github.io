fa8d218d787c6301c6189b0b838921793db97583
Dustin Young <DustinYoung@Dustins-MacBook-Pro-2.local>
2017-02-05 14:39:37 -0800

    udpate

--- js/controllers/SuperController.js
+++ js/controllers/SuperController.js
@@ -1,5 +1,6 @@
 angular.module('app').controller('SuperController', function($scope) {
 
+	var peeps = ['Dustin', 'Brent', 'Brian', 'Chase', 'Perry'];
 
 	$scope.team1 = {
 		nums: [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0]],
@@ -15,8 +16,8 @@ angular.module('app').controller('SuperController', function($scope) {
 
 	$scope.squares = [
 		['', '', '', '', ''],
-		['', '', '', '', ''],
-		['', '', '', '', ''],
+		['', '', 'Brent', '', ''],
+		['', 'Dustin', 'Brian', '', ''],
 		['', '', '', '', ''],
 		['', '', '', '', '']
 	];
