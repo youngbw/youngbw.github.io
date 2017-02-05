080cb2226328a1fd1b625ed96d7e344bf3d53eab
Dustin Young <DustinYoung@Dustins-MacBook-Pro-2.local>
2017-02-05 13:23:51 -0800

    blank slate

--- js/controllers/SuperController.js
+++ js/controllers/SuperController.js
@@ -2,23 +2,23 @@ angular.module('app').controller('SuperController', function($scope) {
 
 
 	$scope.team1 = {
-		nums: [[0, 1], [2, 3], [4, 5], [6, 7], [8, 9]],
+		nums: [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0]],
 		name: 'Patriots',
 		pic: 'images/patriots_super.jpg'
 	};
 
 	$scope.team2 =  {
-		nums: [[0, 1], [2, 3], [4, 5], [6, 7], [8, 9]],
+		nums: [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0]],
 		name: 'Falcons',
 		pic: 'images/falcons_super.jpg'
 	};
 
 	$scope.squares = [
-		['Brent', 'Chase', 'Perry', 'Brian', 'Brent'],
-		['Brent', 'Chase', 'Perry', 'Brian', 'Brent'],
-		['Brent', 'Chase', 'Perry', 'Brian', 'Brent'],
-		['Brent', 'Chase', 'Perry', 'Brian', 'Brent'],
-		['Brent', 'Chase', 'Perry', 'Brian', 'Brent']
+		['', '', '', '', ''],
+		['', '', '', '', ''],
+		['', '', '', '', ''],
+		['', '', '', '', ''],
+		['', '', '', '', '']
 	];
 
 });
