68e97c0e571a4e0ce92dd48e5f16c351ea5673cf
Dustin Young <DustinYoung@Dustins-MacBook-Pro-2.local>
2017-02-05 15:14:56 -0800

    update

--- js/controllers/SuperController.js
+++ js/controllers/SuperController.js
@@ -1,7 +1,7 @@
 angular.module('app').controller('SuperController', function($scope) {
 
 	var peeps = ['Dustin', 'Brent', 'Brian', 'Chase', 'Perry'];
-	var colors = ['pink', 'gold', 'aqua', 'lightgreen', 'red'];
+	var colors = ['pink', 'gold', 'aqua', 'forestgreen', 'red'];
 
 	$scope.team1 = {
 		nums: [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0]],
